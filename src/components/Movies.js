f95eb0de40909e8888b9e66b57c609d8dd154513
jesse <jesse.kibaara@student.moringaschool.com>
2023-02-17 16:52:52 +0300

    syntax errors corrected on movie component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,19 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div><h1>Movies Page</h1>
+  movies.map((movies) => (
+    <div key={movies.title}>
+      <h3>Title: {movies.title}</h3>
+      <p>Time: {movies.time} minutes</p>
+      <p>Genres:</p>
+      <ul>
+        {movies.genres.map((genre, index) => (
+          <li key={index}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))</div>;
 }
 
 export default Movies;
